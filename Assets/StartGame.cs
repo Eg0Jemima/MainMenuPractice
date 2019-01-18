67815387980f4df8ab26fc1cb9f2e62d3a07ba86
DESKTOP-II89TNP\cilde <cildev2000@gmail.com>
2019-01-18 16:33:33 -0500

    Pushing initial project

---
+++ Assets/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !player.activeInHierarchy){
+            player.SetActive(true);
+        }   
+    }
+}
